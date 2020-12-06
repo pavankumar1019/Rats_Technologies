08392efbf126dbc9377533e13ed50ba93f49709b
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-12-06 22:59:40 +0530

    a

---
+++ PHP/sendmail.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['message'])){
+    $to      = 'pavan5639kumar@gmail.com';
+    $subject = "Rats Technologies"; 
+    $message = $_POST['message']; 
+    $headers = "From: ".$_POST['sender_nam‌​e​']." <".$_POST['sender_em‌​ail‌​'].">\r\n"; $headers = "Reply-To: ".$_POST['sender_ema‌​il‌​']."\r\n"; 
+    $headers = "Content-type: text/html; charset=iso-8859-1\r\n";
+    'X-Mailer: PHP/' . phpversion();
+    if(mail($to, $subject, $message, $headers)) echo json_encode(['success'=>true]); 
+    else echo json_encode(['success'=>false]);
+    exit;
+ }
+?>
