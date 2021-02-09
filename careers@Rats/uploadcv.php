c6504ae0746aa32ef9105566d8d998fd2621cce4
pavan5639 <pavan5639kumar@gmail.com>
2021-02-09 12:51:52 +0530

    a

---
+++ careers@Rats/uploadcv.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+if(!isset($_SESSION['userData'])){
+	header('location: index.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>File Upload</title>
+</head>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+<input type="file" name="file" />
+<button type="submit" name="upload">upload</button>
+</form>
+</body>
+</html>
