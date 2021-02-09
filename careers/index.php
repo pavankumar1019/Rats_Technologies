fe01f3d42ff4a6e34243d63f5d6a8e338ea72749
pavan5639 <pavan5639kumar@gmail.com>
2021-02-09 10:42:18 +0530

    Update index.php

--- careers/index.php
+++ careers/index.php
@@ -15,24 +15,24 @@ $loginURL = filter_var($authUrl, FILTER_SANITIZE_URL);
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-	<header>
+	<!-- <header>
 		<div class="head_left">
 			<img src="assets/image/logo.png" height="50px"  >
 		</div>
-	</header>
+	</header> -->
 	<p class="main_title">
 		Login with google account<br/>
-		<a href="http://ratstechnologies.com/"><small>←Back to home page</small></a>
+		<a href="http://ratstechnologies.com/" ><small>←Back to home page</small></a>
 	</p>
 	<section class="main">
 		<div class="inner">
 			<div class="img_p">
-				<img src="../img/rats logo black.png" class="img"> 
+				<img src="../img/rats logo black.png" class="img" width="70px"> 
 			</div>
 			<p class="inner_p">
 				Let visitors easily authorize on your website with their Google account and save and utilize their data:)
 			</p>
-			<a href="<?= htmlspecialchars( $loginURL ); ?>"><img src="assets/image/login-google.png" class="fbbutton" alt="Login With Google"></a>
+			<a href="<?= htmlspecialchars( $loginURL ); ?>"><img src="../img/google.png" class="fbbutton" alt="Login With Google"></a>
 		</div>
 	</section>
 	<!-- <section class="content content--related">
