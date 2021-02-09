64a9bfebbce3082fbfc4aeabb4fe47eb02c609e5
pavan5639 <pavan5639kumar@gmail.com>
2021-02-09 11:39:05 +0530

    Update view.php

--- careers/view.php
+++ careers/view.php
@@ -28,7 +28,7 @@ if(!isset($_SESSION['userData'])){
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="#">Rats Technologies</a>
+    <a class="navbar-brand" href="#">RatsTechnologies</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
