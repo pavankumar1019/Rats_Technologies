d64cd4b66d75190d10fd0e7d8acd6c107052673f
pavan5639 <pavan5639kumar@gmail.com>
2021-02-09 13:32:38 +0530

    Update uploadcv.php

--- careers@Rats/uploadcv.php
+++ careers@Rats/uploadcv.php
@@ -21,35 +21,35 @@ if(!isset($_SESSION['userData'])){
 <div class="container container my-4">
 <form action="upload.php" method="post" enctype="multipart/form-data">
 
-<div class="col-md-6">
+<div class="col-12">
     <label for="inputEmail4" class="form-label">Email</label>
     <input type="email" class="form-control" id="inputEmail4">
   </div>
-  <div class="col-md-6">
+  <div class="col-12">
     <label for="inputPassword4" class="form-label">Contact</label>
     <input type="text" class="form-control" id="inputPassword4">
   </div>
-  <div class="col-6">
+  <div class="col-12">
     <label for="inputAddress" class="form-label">Address</label>
     <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
   </div>
-  <div class="col-6">
+  <div class="col-12">
     <label for="inputAddress" class="form-label">Skills</label>
     <input type="text" class="form-control" id="inputAddress" placeholder="Eg. Java, PHP, Digital Marketing..">
   </div>
-  <div class="col-6">
+  <div class="col-12">
     <label for="inputAddress" class="form-label">languages known</label>
     <input type="text" class="form-control" id="inputAddress" placeholder="Eg. Hindi, Kannada,Tamil..">
   </div>
-  <div class="col-md-6">
+  <div class="col-12">
     <label for="inputCity" class="form-label">City</label>
     <input type="text" class="form-control" id="inputCity">
   </div>
-  <div class="col-md-4">
+  <div class="col-12">
   <label for="inputCity" class="form-label">State</label>
     <input type="text" class="form-control" id="inputCity">
   </div>
-  <div class="col-md-2">
+  <div class="col-12">
   <label for="formFile" class="form-label">Upload Resume/CV</label>
   <input class="form-control"  id="formFile" type="file" name="file" />
   </div>
