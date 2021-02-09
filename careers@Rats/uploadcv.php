c2dfdb8ce04fa291844d08b7bc66e7fcff41e0cb
pavan5639 <pavan5639kumar@gmail.com>
2021-02-09 13:16:13 +0530

    Update uploadcv.php

--- careers@Rats/uploadcv.php
+++ careers@Rats/uploadcv.php
@@ -8,6 +8,9 @@ if(!isset($_SESSION['userData'])){
 <html>
 <head>
 <title>File Upload</title>
+ <!-- Bootstrap CSS -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
 </head>
 <body>
 <form action="upload.php" method="post" enctype="multipart/form-data">
@@ -47,5 +50,7 @@ if(!isset($_SESSION['userData'])){
   <button type="submit" class="btn btn-primary"  name="upload">upload</button>
   </div>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
 </body>
 </html>
